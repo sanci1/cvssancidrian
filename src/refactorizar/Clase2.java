4a49648c978bb41bd29156027c60417f7841d8b5
dam <dam@203P12>
2025-03-13 13:56:09 +0100

    hola

---
+++ src/refactorizar/Clase2.java
@@ -0,0 +1,15 @@
+package refactorizar;
+
+/**
+ *
+ * @author dam
+ */
+public class Clase2 {
+    
+    public Clase2(){
+        
+    int no, se, que, poner;
+    //estamos en la etiqueta
+    
+    }
+}
